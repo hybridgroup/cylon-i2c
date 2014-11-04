b243f5bc04086cf374ba01dd2a7f076f0e604f8e
Andrew Stewart <andrew@stwrt.ca>
2014-11-04 13:11:52 -0800

    Update module registration for latest Cylon changes

--- test/specs/cylon-i2c.spec.js
+++ test/specs/cylon-i2c.spec.js
@@ -11,12 +11,19 @@ var module = source("cylon-i2c");
 var Cylon = require("cylon");
 
 describe("I2C", function() {
+  describe('#drivers', function() {
+    it("returns an array of all drivers the module supports", function() {
+      var drivers = ['blinkm', 'hmc6352', 'mpl115a2', 'bmp180', 'mpu6050', 'lcd', 'lsm9ds0g', 'lsm9ds0xm'];
+      expect(module.drivers).to.be.eql(drivers);
+    });
+  });
+
   describe("#driver", function() {
     var opts = { device: { connection: {} }, extraParams: {} };
 
     context("with 'blinkm'", function() {
       it("returns a BlinkM driver instance", function() {
-        opts.name = 'blinkm';
+        opts.driver = 'blinkm';
         var driver = module.driver(opts);
         expect(driver).to.be.an.instanceOf(BlinkM);
       });
@@ -24,7 +31,7 @@ describe("I2C", function() {
 
     context("with 'hmc6352'", function() {
       it("returns a Hmc6352 driver instance", function() {
-        opts.name = 'hmc6352';
+        opts.driver = 'hmc6352';
         var driver = module.driver(opts);
         expect(driver).to.be.an.instanceOf(Hmc6352);
       });
@@ -32,7 +39,7 @@ describe("I2C", function() {
 
     context("with 'mpl115a2'", function() {
       it("returns a Mpl115A2 driver instance", function() {
-        opts.name = 'mpl115a2';
+        opts.driver = 'mpl115a2';
         var driver = module.driver(opts);
         expect(driver).to.be.an.instanceOf(Mpl115A2);
       });
@@ -40,7 +47,7 @@ describe("I2C", function() {
 
     context("with 'bmp180'", function() {
       it("returns a Bmp180 driver instance", function() {
-        opts.name = 'bmp180';
+        opts.driver = 'bmp180';
         var driver = module.driver(opts);
         expect(driver).to.be.an.instanceOf(Bmp180);
       });
@@ -48,7 +55,7 @@ describe("I2C", function() {
 
     context("with 'mpu6050'", function() {
       it("returns a Mpu6050 driver instance", function() {
-        opts.name = 'mpu6050';
+        opts.driver = 'mpu6050';
         var driver = module.driver(opts);
         expect(driver).to.be.an.instanceOf(Mpu6050);
       });
@@ -56,7 +63,7 @@ describe("I2C", function() {
 
     context("with 'lcd'", function() {
       it("returns a LCD driver instance", function() {
-        opts.name = 'lcd';
+        opts.driver = 'lcd';
         var driver = module.driver(opts);
         expect(driver).to.be.an.instanceOf(LCD);
       });
@@ -67,49 +74,6 @@ describe("I2C", function() {
         var result = module.driver({name: 'notavaliddriver'});
         expect(result).to.be.eql(null);
       });
-    });
-  });
-
-  describe("#register", function() {
-    var robot;
-    beforeEach(function() {
-      robot = { registerDriver: spy() };
-      stub(Cylon.Logger, 'debug');
-      module.register(robot);
-    });
-
-    afterEach(function() {
-      Cylon.Logger.debug.restore();
-    });
-
-    it("registers the BlinkM driver", function() {
-      expect(Cylon.Logger.debug).to.be.calledWithMatch("Registering i2c BlinkM driver");
-      expect(robot.registerDriver).to.be.calledWith("cylon-i2c", "blinkm");
-    });
-
-    it("registers the HMC6352 driver", function() {
-      expect(Cylon.Logger.debug).to.be.calledWithMatch("Registering i2c HMC6352 driver");
-      expect(robot.registerDriver).to.be.calledWith("cylon-i2c", "hmc6352");
-    });
-
-    it("registers the MPL115A2 driver", function() {
-      expect(Cylon.Logger.debug).to.be.calledWithMatch("Registering i2c MPL115A2 driver");
-      expect(robot.registerDriver).to.be.calledWith("cylon-i2c", "mpl115a2");
-    });
-
-    it("registers the BMP180 driver", function() {
-      expect(Cylon.Logger.debug).to.be.calledWithMatch("Registering i2c BMP180 driver");
-      expect(robot.registerDriver).to.be.calledWith("cylon-i2c", "bmp180");
-    });
-
-    it("registers the MPU6050 driver", function() {
-      expect(Cylon.Logger.debug).to.be.calledWithMatch("Registering i2c MPU6050 driver");
-      expect(robot.registerDriver).to.be.calledWith("cylon-i2c", "mpu6050");
-    });
-
-    it("registers the LCD driver", function() {
-      expect(Cylon.Logger.debug).to.be.calledWithMatch("Registering i2c LCD driver");
-      expect(robot.registerDriver).to.be.calledWith("cylon-i2c", "lcd");
     });
   });
 });
