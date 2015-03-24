a04874d1f0fa15ed446ffd499feb807ea44dd347
Andrew Stewart <andrew@stwrt.ca>
2015-03-24 10:03:00 -0700

    Add basic I2CDriver parent class

---
+++ spec/lib/i2c-driver.js
@@ -0,0 +1,40 @@
+// jshint expr:true
+"use strict";
+
+var Cylon = require("cylon");
+
+var I2CDriver = source("i2c-driver");
+
+describe("I2CDriver", function() {
+  var driver;
+
+  beforeEach(function() {
+    driver = new I2CDriver({
+      name: "mpl115a2",
+      address: 0x40
+    });
+  });
+
+  it("is a Cylon driver", function() {
+    expect(driver).to.be.an.instanceOf(I2CDriver);
+    expect(driver).to.be.an.instanceOf(Cylon.Driver);
+  });
+
+  describe("constructor", function() {
+    it("sets @address to provided value", function() {
+      expect(driver.address).to.be.eql(0x40);
+    });
+  });
+
+  it("provides a basic #start method", function() {
+    var callback = spy();
+    driver.start(callback);
+    expect(callback).to.be.called;
+  });
+
+  it("provides a basic #halt method", function() {
+    var callback = spy();
+    driver.halt(callback);
+    expect(callback).to.be.called;
+  });
+});
