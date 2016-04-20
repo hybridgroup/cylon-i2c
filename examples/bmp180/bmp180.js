3519aac8f9c0a2d7fec108c7c9474cf0cb02da5c
deadprogram <ron@hybridgroup.com>
2016-04-20 16:22:45 -0700

    Correct start for BMP180
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ examples/bmp180/bmp180.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var Cylon = require("cylon");
+
+Cylon.robot({
+  connections: {
+    board: { adaptor: "chip" }
+  },
+
+  devices: {
+    bmp180: { driver: "bmp180" }
+  },
+
+  work: function(my) {
+    every((1).seconds(), function() {
+      my.bmp180.getAltitude(0, null, function(err, data) {
+        if (err) { console.error("Error:", err); }
+        console.log(data);
+      });
+      my.bmp180.getTemperature(function(err, data) {
+        if (err) { console.error("Error:", err); }
+        console.log(data);
+      });
+    });
+  }
+}).start();
